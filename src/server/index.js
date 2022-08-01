ae28420c3abe18846a5c883c2c8d63e3abe9e168
Dragon <j0918023423@gmail.com>
2022-08-01 22:31:26 +0800

    response whole html to client

--- src/server/index.js
+++ src/server/index.js
@@ -2,13 +2,23 @@ import express from 'express';
 import React from 'react';
 import App from '../common/App';
 import { renderToString } from 'react-dom/server';
+import fs from 'fs';
 
 const PORT = 3001;
 const server = express();
 
 server.get('/', (req, res) => {
   const html = renderToString(<App />);
-  return res.send(html);
+  fs.readFile('build/index.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
+    }
+    // inject app`s static contents to the div with id equal root
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${html}</div>`)
+    );
+  });
 });
 
 server.use(express.static('build'));
